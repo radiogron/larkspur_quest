eb3bc7d6d95e37054eff9210c1f1e519df904c31
radiogron <arseny@i-am.studio>
2020-07-13 02:32:01 +0300

    Migrated vue config fully from package.json

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/larkspur_quest/'
-        : '/'
+        : '/',
+    pages: {
+        index: {
+            entry: "src/main.js",
+            title: "Larkspur Executive"
+        }
+    }
 }
